2e1ac8ada7c48f8a153e7bd7369f9599e73cbda0
Abhiram_Bikkina <abhiramamrita@gmail.com>
2026-09-06 01:37:20 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,39 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        time, fresh = 0, 0
+        rows, cols = len(grid), len(grid[0])
+        
+        # 1. Count fresh oranges and add all initially rotten oranges to the queue
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+                    
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        
+        # 2. Perform BFS layer by layer
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    
+                    # If out of bounds or not a fresh orange, skip
+                    if (row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != 1):
+                        continue
+                    
+                    # Rot the fresh orange
+                    grid[row][col] = 2    
+                    q.append((row, col))
+                    fresh -= 1
+            
+            # Increment time after processing the current minute's wave of rotting
+            time += 1
+            
+        return time if fresh == 0 else -1
